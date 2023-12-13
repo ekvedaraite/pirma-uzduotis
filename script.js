d10b48b89e33f50fe0beb8c4ce50c5bd3b682775
ekvedaraite <evelinakvedaraite2003@gmail.com>
2023-12-13 12:08:45 +0200

    init

---
+++ script.js
@@ -0,0 +1,31 @@
+/*
+Šablonas fetch
+fetch(url)
+  .then(res => res.json())
+  .then(data => console.log(data))
+  .catch(e => console.log(e.message))
+
+
+1. Jums paskambino pažinčių portalas – jiems reikia staigiai sukurti front-endą, kuris pasiimtų duomenis iš https://randomuser.me/api/ ir juos atvaizduotų panašioje kortelėje kaip čia (dizainas neturi atitikti, bet padarykit tvarkingai - jį galite pilnai su HTML pasirašyti, bet norintiems sudėtingiau - pabandykite su JS sukurti html elementus).
+*/
+fetch('https://randomuser.me/api/')
+  .then(res => res.json())
+  .then(data => {
+    const img = document.createElement('img')
+    img.src = data.results[0].picture.large
+    img.width = 500
+    document.body.append(img)
+
+    const h1 = document.createElement('h1')
+    h1.textContent = `${data.results[0].name.title} ${data.results[0].name.first} ${data.results[0].name.last}`
+    document.body.append(h1)
+
+    const h2 = document.createElement('h2')
+    h2.textContent = `Age: ${data.results[0].dob.age}`
+    document.body.append(h2)
+
+    const p = document.createElement('p')
+    p.textContent = `Email: ${data.results[0].email}`
+    document.body.append(p)
+  })
+  .catch(e => console.log(e.message))
